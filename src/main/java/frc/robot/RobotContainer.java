301009b289d1604bc41985eef0708faaeb5fa6cf
Edwin Yazbec <edwiny@vrs-inc.com>
2025-03-12 10:31:12 -0400

    basic subsystem and command usage in RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,10 +4,15 @@
 
 package frc.robot;
 
+import frc.robot.commands.SetPositionOneSparkMax;
+import frc.robot.commands.SetPositionTwoSparkMax;
+import frc.robot.commands.SetSpeedOneSparkMax;
+import frc.robot.subsystems.PositionalOneSparkMax;
+import frc.robot.subsystems.PositionalTwoSparkMax;
+import frc.robot.subsystems.VelocityOneSparkMax;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -16,36 +21,43 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
+    // The robot's subsystems and commands are defined here...
+    private final PositionalTwoSparkMax elevator = new PositionalTwoSparkMax(5, 6, 0.5, "elevator", 40, false, true, false, 0.1, 0, 0);
+    private final PositionalOneSparkMax wrist = new PositionalOneSparkMax(7, 0.5, "wrist", 40, false, false, 0.1, 0.0, 0.0);
+    private final VelocityOneSparkMax roller = new VelocityOneSparkMax(8, "Intake", 20, false, false);
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController driverController =new CommandXboxController(0);
-  private final CommandXboxController opController = new CommandXboxController(1);
+    // create your commands here
+    private final SetPositionOneSparkMax moveWristToReady =new SetPositionOneSparkMax(wrist, 1);
+    private final SetPositionTwoSparkMax moveElevatorToScoringPose = new SetPositionTwoSparkMax(elevator, 10);
+    private final SetPositionTwoSparkMax moveElevatorToHome = new SetPositionTwoSparkMax(elevator, 0);
+    private final SetSpeedOneSparkMax intake = new SetSpeedOneSparkMax(roller, 0.5);
+    private final SetSpeedOneSparkMax outtake = new SetSpeedOneSparkMax(roller, -0.5);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    // Configure the trigger bindings
-    configureBindings();
-  }
+    // Replace with CommandPS4Controller or CommandJoystick if needed
+    private final CommandXboxController driverController = new CommandXboxController(0);
+    private final CommandXboxController opController = new CommandXboxController(1);
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
-  private void configureBindings() {}
+    /** The container for the robot. Contains subsystems, OI devices, and commands. */
+    public RobotContainer() {
+        // Configure the trigger bindings
+        configureBindings();
+    }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return new InstantCommand();
-  }
+    private void configureBindings() {
+        opController.leftBumper().whileTrue(intake);
+        opController.leftBumper().whileTrue(outtake);
+        opController.a()
+            .onTrue(moveElevatorToScoringPose.andThen(moveWristToReady))
+            .onFalse(moveElevatorToHome);
+    }
+
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand() {
+        // An example command will be run in autonomous
+        return new InstantCommand();
+    }
 }
